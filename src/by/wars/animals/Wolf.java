17b737e616a23a5f4c672d2b55a65cade5453d8d
Pwyll Crusader <pwyllcrusader@gmail.com>
2020-09-09 12:35:12 +0300

    up up

--- src/by/wars/animals/Wolf.java
+++ src/by/wars/animals/Wolf.java
@@ -4,7 +4,7 @@ public class Wolf extends Animal{
 	
 	public Wolf(String name) {
 		setName(name);
-		setStrength(5 + (int) (Math.random() * 5));
+		setStrength(7 + (int) (Math.random() * 5));
 		setStamina(10 + (int) (Math.random() * 10));
 	}
 }
