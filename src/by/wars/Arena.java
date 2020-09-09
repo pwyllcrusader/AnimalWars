7c21271548207aea8dd7f327bf55ea7a7011fa60
Pwyll Crusader <pwyllcrusader@gmail.com>
2020-09-09 12:31:03 +0300

    fix bugs again

--- src/by/wars/Arena.java
+++ src/by/wars/Arena.java
@@ -8,15 +8,16 @@ public class Arena {
 		System.out.println("Сейчас воюют: " + animal1.getName() + " и " + animal2.getName());
 		System.out.println(animal1.getName() + ": жизней - " + animal1.getStamina() + ", сила - " + animal1.getStrength() );
 		System.out.println(animal2.getName() + ": жизней - " + animal2.getStamina() + ", сила - " + animal2.getStrength() );
-		while (animal1.getStamina() > 0 || animal2.getStamina() > 0) {
+		while (animal1.getStamina() > 0 && animal2.getStamina() > 0) {
 			animal1.hit(animal2);
-			if (animal2.getStamina() < 0) {
+			if (animal2.getStamina() <= 0) {
 				System.out.println("Проиграл " + animal2.getName());
 				break;
 			}
 			animal2.hit(animal1);
 			if (animal1.getStamina() < 0) {
 				System.out.println("Проиграл " + animal1.getName());
+				break;
 			}			
 		}		 
 	}
--- src/by/wars/animals/Bear.java
+++ src/by/wars/animals/Bear.java
@@ -4,7 +4,7 @@ public class Bear extends Animal{
 	
 	public Bear(String name) {
 		setName(name);
-		setStrength(7 + (int) Math.random() * 7 );
-		setStamina(12 + (int) Math.random() * 12);
+		setStrength(7 + (int) (Math.random() * 7 ));
+		setStamina(12 + (int) (Math.random() * 12));
 	}
 }
