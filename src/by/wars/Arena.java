f7ee00109dda101ffe6cdc71ec3021be44ccd744
Pwyll Crusader <pwyllcrusader@gmail.com>
2020-09-09 12:32:08 +0300

    up

--- src/by/wars/Arena.java
+++ src/by/wars/Arena.java
@@ -6,8 +6,8 @@ public class Arena {
 	
 	public void fight(Animal animal1, Animal animal2) {
 		System.out.println("Сейчас воюют: " + animal1.getName() + " и " + animal2.getName());
-		System.out.println(animal1.getName() + ": жизней - " + animal1.getStamina() + ", сила - " + animal1.getStrength() );
-		System.out.println(animal2.getName() + ": жизней - " + animal2.getStamina() + ", сила - " + animal2.getStrength() );
+		System.out.println("    " + animal1.getName() + ": жизней - " + animal1.getStamina() + ", сила - " + animal1.getStrength() );
+		System.out.println("    " + animal2.getName() + ": жизней - " + animal2.getStamina() + ", сила - " + animal2.getStrength() );
 		while (animal1.getStamina() > 0 && animal2.getStamina() > 0) {
 			animal1.hit(animal2);
 			if (animal2.getStamina() <= 0) {
