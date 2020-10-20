6ceeaab37cd13a502cf9172ad7dbfe563b21e8be
Pwyll Crusader <pwyllcrusader@gmail.com>
2020-10-20 18:31:33 +0300

    done again

--- api-module/src/main/java/com/wars/api/service/IAnimalService.java
+++ api-module/src/main/java/com/wars/api/service/IAnimalService.java
@@ -5,8 +5,8 @@ import java.util.List;
 import com.wars.entities.Animal;
 
 public interface IAnimalService {
-	
+
 	void addFighter(Animal animal);
-	
+
 	List<Animal> getFighters();
 }
--- api-module/src/main/java/com/wars/api/service/IWarService.java
+++ api-module/src/main/java/com/wars/api/service/IWarService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.wars.entities.Animal;
 
 public interface IWarService {
-	
+
 	void hit(Animal animal1, Animal animal2);
 
 	void fight(Animal animal1, Animal animal2);
